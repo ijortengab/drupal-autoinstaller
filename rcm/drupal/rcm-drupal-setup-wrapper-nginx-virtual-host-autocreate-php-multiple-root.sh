8a7ef3c6a903a5732854f15a141c737af0b56649
IjorTengab <m_roji28@yahoo.com>
2024-12-28 08:15:14 +0700

    Menghapus unused function fileMustExists, dan isFileExists.

--- rcm/drupal/rcm-drupal-setup-wrapper-nginx-virtual-host-autocreate-php-multiple-root.sh
+++ rcm/drupal/rcm-drupal-setup-wrapper-nginx-virtual-host-autocreate-php-multiple-root.sh
@@ -277,30 +277,6 @@ backupDir() {
     fi
     mv "$oldpath" "$newpath"
 }
-fileMustExists() {
-    # global used:
-    # global modified:
-    # function used: __, success, error, x
-    if [ -f "$1" ];then
-        __; green File '`'$(basename "$1")'`' ditemukan.; _.
-    else
-        __; red File '`'$(basename "$1")'`' tidak ditemukan.; x
-    fi
-}
-isFileExists() {
-    # global used:
-    # global modified: found, notfound
-    # function used: __
-    found=
-    notfound=
-    if [ -f "$1" ];then
-        __ File '`'$(basename "$1")'`' ditemukan.
-        found=1
-    else
-        __ File '`'$(basename "$1")'`' tidak ditemukan.
-        notfound=1
-    fi
-}
 validateMachineName() {
     local value="$1" _value
     local parameter="$2"
