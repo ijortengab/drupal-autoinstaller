297243554f264072eae160257306cf009fce5ca4
ijortengab <m_roji28@yahoo.com>
2025-05-09 21:17:36 +0700

    [rcm-php-setup-drupal.sh] Menambah dependency rcm-php-apt. Menghapus function downloadApplication, validateApplication. Menghapus unused function addRepositoryPpaOndrejPhp.

--- rcm/php/rcm-php-setup-drupal.sh
+++ rcm/php/rcm-php-setup-drupal.sh
@@ -62,6 +62,9 @@ Global Options.
         Print version of this script.
    --help
         Show this help.
+
+Dependency:
+   rcm-php-apt
 EOF
 }
 
@@ -80,79 +83,33 @@ while IFS= read -r line; do
     [[ -z "$line" ]] || command -v `cut -d: -f1 <<< "${line}"` >/dev/null || { error Unable to proceed, command not found: '`'`cut -d: -f1 <<< "${line}"`'`'.; x; }
 done <<< `printHelp 2>/dev/null | sed -n '/^Dependency:/,$p' | sed -n '2,/^\s*$/p' | sed 's/^ *//g'`
 
-# Functions.
-downloadApplication() {
-    local aptnotfound=
-    local string_quoted
-    chapter Melakukan instalasi aplikasi.
-    code apt install "$@"
-    [ -z "$aptinstalled" ] && aptinstalled=$(apt --installed list 2>/dev/null)
-    for i in "$@"; do
-        string_quoted=$(sed "s/\./\\\./g" <<< "$i")
-        if ! grep -q "^$string_quoted/" <<< "$aptinstalled";then
-            aptnotfound+=" $i"
-        fi
-    done
-    if [ -n "$aptnotfound" ];then
-        __ Menginstal.
-        code apt install -y"$aptnotfound"
-        apt install -y --no-install-recommends $aptnotfound
-        aptinstalled=$(apt --installed list 2>/dev/null)
-    else
-        __ Aplikasi sudah terinstall seluruhnya.
-    fi
-}
-validateApplication() {
-    local aptnotfound=
-    for i in "$@"; do
-        if ! grep -q "^$i/" <<< "$aptinstalled";then
-            aptnotfound+=" $i"
-        fi
-    done
-    if [ -n "$aptnotfound" ];then
-        __; red Gagal menginstall aplikasi:"$aptnotfound"; x
-    fi
-}
-addRepositoryPpaOndrejPhp() {
-    local notfound=
-    local string string_quoted
-    chapter Mengecek source PPA ondrej/php
-    # Based on https://packages.sury.org/php/README.txt
-    cd /etc/apt/sources.list.d
-    string='https://packages.sury.org/php/'
-    string_quoted=$(sed "s/\./\\\./g" <<< "$string")
-    if grep --no-filename -R -E "$string_quoted" | grep -q -v -E '^\s*#';then
-        __ Sudah terdapat di direktori '`'/etc/apt/sources.list.d'`'.
-    else
-        notfound=1
-        __ Tidak terdapat di direktori '`'/etc/apt/sources.list.d'`'.
-    fi
-    cd - >/dev/null
-    ____
-
-    if [ -n "$notfound" ];then
-        chapter Menambahkan source PPA ondrej/php
-        curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
-        sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
-        apt update -y
-        if grep --no-filename -R -E "$string_quoted" | grep -q -v -E '^\s*#';then
-            __; green Sudah terdapat di direktori '`'/etc/apt/sources.list.d'`'.; _.
-        else
-            __; red Tidak terdapat di direktori '`'/etc/apt/sources.list.d'`'.;  x
-        fi
-        ____
-    fi
-}
-
 # Require, validate, and populate value.
 chapter Dump variable.
+[ -n "$fast" ] && isfast=' --fast' || isfast=''
 code 'php_version="'$php_version'"'
 ____
 
-chapter Instalasi PHP Extension.
-downloadApplication php"$php_version"-{common,gd,mysql,imap,cli,fpm,curl,intl,pspell,sqlite3,tidy,xmlrpc,xsl,zip,mbstring,soap,opcache}
-validateApplication php"$php_version"-{common,gd,mysql,imap,cli,fpm,curl,intl,pspell,sqlite3,tidy,xmlrpc,xsl,zip,mbstring,soap,opcache}
-____
+INDENT+="    " \
+rcm-php-apt $isfast \
+    --php-version="$php_version" \
+    --extension=common \
+    --extension=gd \
+    --extension=mysql \
+    --extension=imap \
+    --extension=cli \
+    --extension=fpm \
+    --extension=curl \
+    --extension=intl \
+    --extension=pspell \
+    --extension=sqlite3 \
+    --extension=tidy \
+    --extension=xmlrpc \
+    --extension=xsl \
+    --extension=zip \
+    --extension=mbstring \
+    --extension=soap \
+    --extension=opcache \
+    ; [ ! $? -eq 0 ] && x
 
 exit 0
 
